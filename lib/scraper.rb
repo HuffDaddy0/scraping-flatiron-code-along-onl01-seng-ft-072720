e2f4093842177b44c1b144c646a031e568a30cdb
Tyler Huffman <ty.huff24@gmail.com>
2020-08-12 23:37:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -31,5 +31,4 @@ end
 
 end
 
-scraper.new.get_page
-
+Scraper.new.get_page
